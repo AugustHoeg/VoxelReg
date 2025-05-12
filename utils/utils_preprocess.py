f6b1d627a4f7d37ed2e4d022a261230b6ac21a77
AugustHoeg <aulho@dtu.dk>
2025-05-12 11:16:04 +0200

    'update'

--- utils/utils_preprocess.py
+++ utils/utils_preprocess.py
@@ -241,7 +241,7 @@ def save_image_pyramid(image_pyramid, mask_pyramid, affines, scan_path, out_path
         print("Saving image for pyramid level: ", i)
         write_nifti(image_pyramid[i], affines[i], os.path.join(out_path, out_name + f"_scale_{2 ** i}.nii.gz"))
 
-    for i in range(2, len(mask_pyramid)):
+    for i in range(0, len(mask_pyramid)):
         # np.save(os.path.join(out_path, out_name + f"_scale_{2 ** i}_mask.npy"), mask)
         # write_tiff(mask, os.path.join(sample_path, filename + "_mask.tiff"))
         print("Saving mask for pyramid level: ", i)
