0b9aa74bf47b934b9233c3d4526be049ffe7f4f0
AugustHoeg <aulho@dtu.dk>
2025-07-15 10:20:06 +0200

    'update'

--- preprocess.py
+++ preprocess.py
@@ -1,37 +1,48 @@
 import os
+import os
 import argparse
 
 from utils.utils_plot import viz_slices, viz_multiple_images
-from utils.utils_preprocess import crop_to_roi, preprocess
+from utils.utils_preprocess import crop_to_roi, preprocess, get_image_and_affine, save_image_pyramid, get_image_pyramid, define_image_space
+from utils.utils_nifti import voxel2world, set_origin
 
-project_path = "C:/Users/aulho/OneDrive - Danmarks Tekniske Universitet/Dokumenter/Github/Vedrana_master_project/3D_datasets/datasets/VoDaSuRe/"
-sample_path = project_path + "Larch_A_bin1x1/"
+# Define paths
+project_path = "C:/Users/aulho/OneDrive - Danmarks Tekniske Universitet/Dokumenter/Github/Vedrana_master_project/3D_datasets/datasets/2022_QIM_52_Bone/"
 
-scan_path = sample_path + "Larch_A_bin1x1_LFOV_80kV_7W_air_2p5s_6p6mu_bin1_recon.txm"  # LR image is the moving image
-out_path = project_path + "stitched/processed/" + "Larch_A_bin1x1/"
-out_name = "Larch_A_LFOV_pos1"  # Name of the output file
+# Define paths
+sample_path = project_path + "femur_001/"
+moving_path = sample_path + "clinical/volume/f_001.nii"
+fixed_path = sample_path + "micro/volume/f_001.nii"
+out_name = "f_001_prepropress"  # Name of the output file
 
-#scan_path = sample_path + "Larch_A_bin1x1_4X_80kV_7W_air_1p5_1p67mu_bin1_pos1_recon.tif"  # HR image is the reference
-#out_name = "Larch_A_4x_pos1"  # Name of the output file
 
 def parse_arguments():
 
     # Set up argument parser
     parser = argparse.ArgumentParser(description="Preprocess 3D image data for registration.")
     parser.add_argument("--sample_path", type=str, required=False, help="Path to the sample directory.")
-    parser.add_argument("--scan_path", type=str, required=False, help="Path to the scan file.")
-    parser.add_argument("--out_path", type=str, required=False, help="Path to the output file.")
-    parser.add_argument("--out_name", type=str, required=False, help="Output name for the processed image.")
+
+    parser.add_argument("--moving_path", type=str, required=False, help="Path to the scan file.")
+
+    parser.add_argument("--moving_out_path", type=str, required=False, help="Path to the output file.")
+    parser.add_argument("--moving_out_name", type=str, required=False, default="moving", help="Output name for the processed image.")
+
     parser.add_argument("--run_type", type=str, default="HOME PC", help="Run type: HOME PC or DTU HPC.")
 
-    parser.add_argument("--min_size", type=int, nargs=3, default=(1980, 800, 800), help="Minimum size for cropping.")
-    parser.add_argument("--max_size", type=int, nargs=3, default=(1980, 800, 800), help="Maximum size for cropping.")
+    parser.add_argument("--moving_min_size", type=int, nargs=3, default=(1600, 800, 800), help="Minimum size for cropping.")
+    parser.add_argument("--moving_max_size", type=int, nargs=3, default=(1600, 800, 800), help="Maximum size for cropping.")
+
+    parser.add_argument("--moving_pixel_size", type=float, nargs=3, default=(None, None, None), help="Pixel size in mm for moving image.")
 
     parser.add_argument("--margin_percent", type=float, default=0.5, help="Margin percentage for cropping.")
-    parser.add_argument("--divis_factor", type=int, default=2, help="Divisibility factor for cropping.")
-    parser.add_argument("--pyramid_depth", type=int, default=3, help="Depth of saved image pyramid.")
-    parser.add_argument("--f", type=int, default=4, help="Resolution factor.")
-    parser.add_argument("--mask_threshold", default=None, help="Threshold for binary mask image, default is None.")
+
+    parser.add_argument("--moving_pyramid_depth", type=int, default=3, help="Depth of saved image pyramid.")
+
+    parser.add_argument("--f", type=int, default=4, help="LR resolution factor.")
+
+    parser.add_argument("--moving_mask_threshold", default=None, help="Threshold for binary mask image, default is None.")
+
+    parser.add_argument("--top_index", type=str, default="last", help="Index for the top slice of the image, default is 'last'")
 
     args = parser.parse_args()
     return args
@@ -50,60 +61,42 @@ if __name__ == "__main__":
     if args.sample_path is not None:
         sample_path = os.path.join(project_path, args.sample_path)
         print("Sample path: ", sample_path)
-    if args.scan_path is not None:
-        scan_path = os.path.join(sample_path, args.scan_path)
-        print("Scan path: ", scan_path)
-    if args.out_path is not None:
-        out_path = os.path.join(sample_path, args.out_path)  # os.path.join(sample_path, args.out_name)
-        print("Output path: ", out_path)
-    if args.out_name is not None:
-        out_name = args.out_name  # os.path.join(sample_path, args.out_name)
-        print("Output name: ", out_name)
+    if args.moving_path is not None:
+        moving_path = os.path.join(sample_path, args.moving_path)
+        print("Moving path: ", moving_path)
+    if args.fixed_path is not None:
+        fixed_path = os.path.join(sample_path, args.fixed_path)
+        print("Fixed path: ", fixed_path)
+    if args.moving_out_path is not None:
+        moving_out_path = os.path.join(sample_path, args.moving_out_path)  # os.path.join(sample_path, args.out_name)
+        print("Moving output path: ", moving_out_path)
+    if args.fixed_out_path is not None:
+        fixed_out_path = os.path.join(sample_path, args.fixed_out_path)  # os.path.join(sample_path, args.out_name)
+        print("Fixed output path: ", fixed_out_path)
 
-    # Define other parameters
-    min_size = tuple(args.min_size)
-    max_size = tuple(args.max_size)
-    f = args.f  # Resolution factor between the two images
-    margin_percent = args.margin_percent  # X% size increase margin of moving image
-    divis_factor = args.divis_factor  # Ensure shape is divisible by d
-    mask_threshold = args.mask_threshold  # Threshold for masking
-    pyramid_depth = args.pyramid_depth  # Save downscaled image
 
-    pyramid, affines = preprocess(scan_path, out_path, out_name, f, margin_percent, divis_factor, min_size, max_size, pyramid_depth, mask_threshold)  # Preprocess moving image
+    ##################### MOVING IMAGE ######################
+
+    # Load moving image
+    moving, moving_affine = get_image_and_affine(moving_path, custom_origin=(0, 0, 0), pixel_size_mm=args.moving_pixel_size)
+
+    # Define moving image space
+    moving, moving_affine, moving_crop_start, moving_crop_end = define_image_space(moving, moving_affine, f=args.f,
+                                                                                   min_size=args.moving_min_size,
+                                                                                   max_size=args.moving_max_size,
+                                                                                   margin_percent=args.margin_percent,
+                                                                                   divis_factor=4,
+                                                                                   top_index=args.top_index)
+
+    # Get & save moving image pyramid
+    pyramid, mask_pyramid, affines = get_image_pyramid(moving, moving_affine, args.moving_pyramid_depth, args.moving_mask_threshold)
+    save_image_pyramid(pyramid, mask_pyramid, affines, moving_path, moving_out_path, args.moving_out_name)
 
     # Visualize
     for i, image in enumerate(pyramid):
-        slices = [image.shape[0] - 16, image.shape[0] - 20, image.shape[0] - 24]
-        viz_slices(image, slices, savefig=True, title=out_name + f"scale_{2**i}_preprocessed")
-
-        #if down is not None:
-        #    slices = [down.shape[0] - 2, down.shape[0] - 4, down.shape[0] - 6]
-        #    viz_slices(down, slices, savefig=True, title=out_name + "_down_4_preprocessed")
-
-    #
-    # # Load moving image tiff as dask array, assuming everything fits in RAM
-    # moving = load_tiff(moving_path)
-    # moving = crop_to_roi(moving, roi_factor=f, margin_percent=0.5, divis_factor=d,
-    #                      minimum_size=(2000, 800, 800), maximum_size=(2000, 800, 800))  # Reduce size
-    #
-    # write_tiff(moving, sample_path + "Larch_A_LFOV_crop_full_height.tiff")
-    # write_nifti(moving, sample_path + "Larch_A_LFOV_crop_full_height.nii.gz")
-    # del moving
-    #
-    # fixed = load_tiff(fixed_path)
-    # fixed = crop_to_roi(fixed, roi_factor=1, margin_percent=0, divis_factor=d,
-    #                     minimum_size=(1900, 1900, 1900), maximum_size=(1900, 1900, 1900))  # Reduce size
-    #
-    #
-    #
-    # # Downsample fixed w. torch
-    # #pool = torch.nn.AvgPool3d(kernel_size=4, stride=4)
-    # #downscaled = pool(torch.from_numpy(fixed).unsqueeze(0).float())
-    #
-    # down_2 = downscale_local_mean(fixed, (2, 2, 2))
-    # down_4 = downscale_local_mean(down_2, (2, 2, 2))
-    #
-    # write_tiff(down_4.astype(np.uint16), sample_path + "Larch_A_4x_pos1_down_4.tiff")
-    # write_nifti(down_4.astype(np.uint16), sample_path + "Larch_A_4x_pos1_down_4.nii.gz")
+        slices = [image.shape[0]//2, image.shape[1]//2, image.shape[2]//2]
+        viz_slices(image, slices[0], save_dir=moving_out_path, title=args.moving_out_name + f"_scale_{2 ** i}_pre_axis_0", axis=0)
+        viz_slices(image, slices[1], save_dir=moving_out_path, title=args.moving_out_name + f"_scale_{2 ** i}_pre_axis_1", axis=1)
+        viz_slices(image, slices[2], save_dir=moving_out_path, title=args.moving_out_name + f"_scale_{2 ** i}_pre_axis_2", axis=2)
 
     print("Done")
