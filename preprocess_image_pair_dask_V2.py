fb17380f94dc19063b9f68602dd0fbcdeed49eb6
AugustHoeg <aulho@dtu.dk>
2026-02-04 13:35:24 +0100

    'update'

--- preprocess_image_pair_dask_V2.py
+++ preprocess_image_pair_dask_V2.py
@@ -146,10 +146,10 @@ if __name__ == "__main__":
     print("Visualization is set to: ", visualize)
 
     ##################### DASK CLUSTER ######################
-    # cluster = LocalCluster(args.num_workers,
-    #                        threads_per_worker=1,
-    #                        memory_limit=args.memory_limit)
-    # client = Client(cluster)
+    cluster = LocalCluster(args.num_workers,
+                           threads_per_worker=1,
+                           memory_limit=args.memory_limit)
+    client = Client(cluster)
 
 
     ##################### MOVING IMAGE ######################
